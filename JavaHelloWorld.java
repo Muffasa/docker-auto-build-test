b572dbfb8fec71b63ae406f925ad75f4466473e7
Muffasa <ohadgalor1990@gmail.com>
2015-11-20 15:37:04 +0200

    first commit

---
+++ JavaHelloWorld.java
@@ -0,0 +1,10 @@
+public class JavaHelloWorld
+{
+		public static void main (String [] args)
+		{
+			System.out.println("JavaHellowzzzzz");
+			System.out.println("and hloooooowz again");
+		}
+	
+}
+
